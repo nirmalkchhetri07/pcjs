ca27cbd32e4a18c98730aff5e339ee0e3bda427d
nirmalkchhetri07 <nirmalkchhetri07@gmail.com>
2024-11-23 12:10:44 +0545

    loop in js

---
+++ index.js
@@ -0,0 +1,44 @@
+// for loop
+
+let a = 100;
+
+/*for (let i = 0; i < 100; i++) {
+  console.log(a);
+}
+  */
+
+// for in loop
+
+/*
+let obj = {
+  name: "Nirmal",
+  role: "Student",
+};
+
+for (const key in obj) {
+  console.log(key);
+}
+
+// output only key
+// name
+// role
+
+*/
+
+// For of loop
+
+/*
+
+for (const c of "harry") {
+  console.log(c);
+}
+
+*/
+
+// While loop
+
+let i = 0;
+while (i < 6) {
+  console.log(i);
+  i++;
+}
